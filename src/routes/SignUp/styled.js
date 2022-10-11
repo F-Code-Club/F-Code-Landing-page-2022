92daa9760d6307471c23874284acdb6625025840
Ducbao <baondforwork@gmail.com>
2022-10-11 18:59:53 +0700

    COMPLETE-Adding layout

---
+++ src/routes/SignUp/styled.js
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    max-width: 90vw;
+    width: 100%;
+    margin-top: 120px;
+`;
+export const Hero = styled.h1`
+    text-align: center;
+    font-family: 'GT Walsheim Pro';
+    font-style: normal;
+    font-weight: 900;
+    font-size: 60px;
+    line-height: 65px;
+
+    /* Black/400 */
+
+    color: #1b1b1b;
+    span {
+        color: #45ce7c;
+    }
+    margin-bottom: 24px;
+`;
+export const Description = styled.h5`
+    font-family: 'GT Walsheim Pro';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    margin-bottom: 48px;
+    text-align: center;
+    color: #6b7280;
+    span {
+        color: #45ce7c;
+    }
+`;
+export const Step = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+export const Stage = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: linear-gradient(153.18deg, rgba(56, 255, 76, 0.2) 0%, rgba(81, 223, 95, 0) 167.08%);
+    backdrop-filter: blur(5px);
+    color: #45ce7c;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 12px;
+`;
+export const Bar = styled.div`
+    width: 90%;
+    height: 2px;
+    background: rgba(171, 171, 171, 0.38);
+`;
+export const Box = styled.div`
+    width: 40px;
+`;
+export const Name = styled.div`
+    max-width: 128px;
+    text-align: center;
+    font-family: 'GT Walsheim Pro';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 150%;
+    color: #00d17d;
+`;
+export const NameContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`;
+export const Button = styled.button`
+    background: linear-gradient(152.97deg, rgba(105, 105, 105, 0.2) 0%, rgba(21, 21, 21, 0) 100%);
+    border-radius: 8px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 12px 20px;
+    width: 194.5px;
+    height: 48px;
+    cursor: pointer;
+`;
+export const Img = styled.img`
+    width: 30px;
+    background: white;
+    border-radius: 50%;
+    margin-right: 12px;
+`;
+export const Google = styled.b`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 150%;
+`;
+export const GoogleContainer = styled.div`
+    margin-top: 40px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ import Activities from './Activities';
 import Home from './Home';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-import Timeline from './TimeLine';
+import SignUp from './SignUp';
 
 const publicRoute = [
     {
@@ -21,8 +21,8 @@ const publicRoute = [
         restrict: true,
     },
     {
-        path: '/timeline',
-        component: <Timeline />,
+        path: '/signup',
+        component: <SignUp />,
         exact: true,
         restrict: true,
     },
