5f25764d4ca6229e2b44081e4a17a4998e2816d8
ListKelvin <phamm5687@gmai.com>
2022-10-24 13:29:37 +0700

    fix navbar

--- src/components/Header/header.style.js
+++ src/components/Header/header.style.js
@@ -16,8 +16,13 @@ const StyledHeader = styled.div`
     padding: 0px 64px;
     align-items: center;
     background: transparent;
+    transition: transform 0.5s ease-in-out;
     /* box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; */
+    &.nav-bar--hidden {
+        transition: all 0.5s;
 
+        transform: translateY(-150%);
+    }
     @media (max-width: 1440px) {
         padding: 8px;
     }
@@ -27,7 +32,7 @@ const StyledHeader = styled.div`
             rgba(21, 21, 21, 0.08) 0%,
             rgba(21, 21, 21, 0.09) 100%
         );
-        height: 100px;
+        height: 80px;
 
         backdrop-filter: blur(10px);
     }
