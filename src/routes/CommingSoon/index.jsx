dfc1db782a1cae2adf9919cca558b1599f405348
List Kelvin <phamm5687@gmail.com>
2022-11-10 19:39:24 +0700

    last update

--- src/routes/CommingSoon/index.jsx
+++ src/routes/CommingSoon/index.jsx
@@ -1,17 +1,22 @@
-const CommingSoon = () => {
-    const styles = {
-        marginTop: '120px',
-        fontSize: '60px',
-        marginBottom: '6px',
-    };
+import { down } from 'styled-breakpoints';
+import styled from 'styled-components';
 
+const CommingSoon = () => {
     const styles2 = { color: '#6B7280', marginBottom: '120px' };
     return (
         <div style={{ textAlign: 'center' }}>
-            <h1 style={styles}>Ending Registration</h1>
+            <Styles>Ending Registration</Styles>
             <p style={styles2}>Thanks for Joining</p>
         </div>
     );
 };
 
 export default CommingSoon;
+export const Styles = styled.h1`
+    margin-top: 120px;
+    font-size: 60px;
+    margin-bottom: 6px;
+    ${down('sm')} {
+        font-size: 40px;
+    }
+`;
